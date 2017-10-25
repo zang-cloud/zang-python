265c5dce878ef17ccc825ba2db4b326f326bf732
calvincertuche <calvincertuche@users.noreply.github.com>
2017-10-25 14:55:43 -0400

    Update available_phone_numbers_example.py

--- docs/examples/available_phone_numbers_example.py
+++ docs/examples/available_phone_numbers_example.py
@@ -4,7 +4,7 @@ from zang.configuration.configuration import Configuration
 from zang.connectors.connector_factory import ConnectorFactory
 from zang.domain.enums.available_number_type import AvailableNumberType
 
-from docs.examples.credetnials import sid, authToken
+from docs.examples.credentials import sid, authToken
 url = 'http://api.zang.io/v2'
 
 configuration = Configuration(sid, authToken, url=url)
