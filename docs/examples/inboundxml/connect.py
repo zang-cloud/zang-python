af492c75504f88876206f8d638c113933ef85ea3
Ajitha Yasmin <s92@avaya.com>
2020-12-08 15:16:07 +0530

    [ZANGCPAAS-1899] Review comments addressed

--- docs/examples/inboundxml/connect.py
+++ docs/examples/inboundxml/connect.py
@@ -1,7 +1,6 @@
 from zang.inboundxml import Response, Connect, Agent
 
 from zang.domain.enums.http_method import HttpMethod
-from zang.inboundxml import Agent
 
 connect = Connect("http://example.com/example-callback-url/say?example=simple.xml",
                 method=HttpMethod.GET)
